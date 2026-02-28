703f8cd189bd88285fae4c59f0f0b6dced8dfa31
Rashmi-Rawat0515 <rashmirawat0515@gmail.com>
2026-02-28 23:46:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pickle
 import string
 import nltk
 nltk.download('punkt_tab')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 ps = PorterStemmer()
@@ -49,4 +50,5 @@ if st.button("Predict"):
  else:
 
     st.header("Not Spam")
+
 
